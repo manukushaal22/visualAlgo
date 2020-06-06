156d34e35be1a5ba37c85aa598b9ad59774cb442
kushaalhulsoor <kushaal.hulsoor@cleartrip.com>
2020-06-06 21:35:13 +0530

    table to divs

--- helpers.js
+++ helpers.js
@@ -12,13 +12,27 @@ function create_table(rows,cols){
     document.body.appendChild(table);
 }
 
+function create_histogram(arr){
+    let histogram = document.createElement('div');
+    histogram.classList.add("histogram");
+    for (let i = 0; i < arr.length; i++){
+        let bar = document.createElement('div');
+        bar.classList.add("histobar");
+        bar.classList.add("bar_index_"+i);
+        histogram.appendChild(bar);
+    }
+    document.body.appendChild(histogram);
+    for(let i=0; i < arr.length; i++){
+        put_bar(arr[i], i);
+    }
+}
+
 function swap_bars(position1, position2){
     let tmp = current_list_data[position1];
     current_list_data[position1] = current_list_data[position2];
     current_list_data[position2] = tmp;
     put_bar(current_list_data[position1], position1);
     put_bar(tmp, position2);
-    
 }
 
 function initialize_table_classes(){
@@ -28,12 +42,7 @@ function initialize_table_classes(){
 }
 
 function put_bar(length, position){
-    for(let i= tot_rows - 1; i >= tot_rows - length; i--){
-        $(".cell_"+i+"_"+position).addClass("cellselect");
-    }
-    for(let i= 0; i < tot_rows - length; i++){
-        $(".cell_"+i+"_"+position).removeClass("cellselect");
-    }
+    $(".bar_index_"+position).css("width", (length)+"px");
 }
 
 function make_histogram(arr){
@@ -48,10 +57,24 @@ function bubbleSort(inputArr){
         for (let j = 0; j < len; j++) {
             if (inputArr[j] > inputArr[j + 1]) {
                 swap_bars(j, j+1);
-                wait(100);
+                wait(1);
             }
         }
     }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function create_random_array(size, min, max){
+    let ar = [];
+    for(let i=0; i < size; i++){
+        ar.push(getRandomInt(min,max));
+    }
+    return ar;
 }
 
 function wait(ms) {
